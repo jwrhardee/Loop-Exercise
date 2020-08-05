1f7a34c6efeed98836955495e92c8d1c150c7158
jesseHardee <jwrhardee@gmail.com>
2020-08-05 21:11:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+# for i in range(1,7):
+#   for j in range (1,i):
+#   print(j,end="")
+
+
+
+
+
+# for b in range(5):
+#   for c in range(4):
+#     print ("hello")
+num = input ("enter a number ")
+num = float(num)
+# conditanial to assess wetar a number posisitve negative or 0
+if num > 0:
+  print ("it is posisitve")
+
+else:
+  if num < 0:
+    print ("it is negative")
+
+if num == 0:
+  print ("it is 0 ")
